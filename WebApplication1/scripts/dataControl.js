024fadcf4585e8976b6575b2bed144ed79e80151
djackson <djackson@DJACKSON>
2017-02-09 16:42:28 -0500

    Copy and pasted all from the other project which was not uploading for some reason.

---
+++ WebApplication1/scripts/dataControl.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="jquery-3.1.1.js"/>
+// The above clode blew my mind, never knew 3 slashes existed to where you
+// could reference a JS file like that to use jQuery in an external JS file.
+
+
+// Below here is a working main with jQuery functioning.
+($(function main() {
+
+}))();
