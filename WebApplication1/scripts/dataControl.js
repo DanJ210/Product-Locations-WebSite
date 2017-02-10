be24615fce347ae84eb07fbbc88c5d8c5f7f3303
Daniel Jackson <danj210@hotmail.com>
2017-02-10 13:35:43 -0500

    Setting up logic in javascripts.

--- WebApplication1/scripts/dataControl.js
+++ WebApplication1/scripts/dataControl.js
@@ -21,17 +21,61 @@
         $('.statusType').text();
         var product = $('#productSelect').val();
         var server = $('.server').val();
-
+        var serverChoice = checkRadioSelect();
         if (product === '1') {
-            server = $('.server').val();
-            $('#1').text(server);
-        } else {
-            server = $('.server').val();
-            $('#2').text(server);
+            if (serverChoice === 'primary') {
+                $('#primary1').text(server);
+            } else if (serverChoice === 'secondary') {
+                $('#secondary1').text(server);
+            }
+            
+        } else if (product === '2') {
+            $('#primary2').text(server);
+        } else if (product === '3') {
+            $('#primary3').text(server);
+        } else if (product === '4') {
+            $('#primary4').text(server);
+        } else if (product === '5') {
+            $('#primary5').text(server);
+        } else if (product === '6') {
+            $('#primary6').text(server);
+        } else if (product === '7') {
+            $('#primary7').text(server);
+        } else if (product === '8') {
+            $('#primary8').text(server);
+        } else if (product === '9') {
+            $('#primary9').text(server);
+        } else if (product === '10') {
+            $('#primary10').text(server);
+        } else if (product === '11') {
+            $('#primary11').text(server);
+        } else if (product === '12') {
+            $('#primary12').text(server);
+        } else if (product === '13') {
+            $('#primary13').text(server);
+        } else if (product === '14') {
+            $('#primary14').text(server);
         }
-        //alert(product);
-     });
 
+        //alert(product);
+    });
+    /*
+    switch (product) {
+        case (product === '1') {
+
+        }
+    }
+    */
+    function checkRadioSelect() {
+        var chosenServer = $('.serverRadio').val();
+        var choice;
+        if (chosenServer === 'server1') {
+            choice = 'primary';
+        } else if (chosenServer === 'server2') {
+            choice = 'secondary';
+        }
+        return choice;
+    }
     //alert(typeof ('.statusType'));
 
 }))();
