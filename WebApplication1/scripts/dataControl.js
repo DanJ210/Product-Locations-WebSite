2d5089ed04c5b0c345ec1b843f6b4e52802782f0
djackson <djackson@DJACKSON>
2017-02-15 15:53:14 -0500

    Checking for consistency.

--- WebApplication1/scripts/dataControl.js
+++ WebApplication1/scripts/dataControl.js
@@ -51,8 +51,6 @@ $('#jsSubmit').click(function() {
         });
         alert("Finished");
     });
-    // Why am I not seeing the php files I made
-
     $('.saveButton').click(function () {
         
     
