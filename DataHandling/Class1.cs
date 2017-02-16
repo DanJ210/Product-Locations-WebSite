8eb70317a9a59df5805b547a902feec0eb2ea6a6
djackson <djackson@DJACKSON>
2017-02-16 13:37:34 -0500

    Changed spacing

--- DataHandling/Class1.cs
+++ DataHandling/Class1.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DataHandling
-{
-    public class Class1
-    {
-        public Class1()
-        {
+namespace DataHandling {
+    public class Class1 {
+        public Class1() {
+            Console.Write("test");
         }
     }
 }
