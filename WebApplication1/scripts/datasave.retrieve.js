da48af469364d47b52bb266462de50d11409fedf
Daniel Jackson <danj210@hotmail.com>
2017-02-21 17:02:00 -0500

    Turned saving data into a module.

--- WebApplication1/scripts/datasave.retrieve.js
+++ WebApplication1/scripts/datasave.retrieve.js
@@ -1,53 +1,55 @@
 /// <reference path="scripts/jquery-3.1.1.js"/>
 
 
-/**
-    * 
-    * @function document ready button click funtion
-    * @description Sends http request when event is triggered to GET data from JSON file.
-    * @param 
-*/
+(function () {
+    
+});
+// Creating a module variable that stores the function to GET data from a JSON file
+// Trying to keep it as private and secure as possible.
+var SendDataModule = (function () {
+    var getSavedTableJSON = new XMLHttpRequest();
+    getSavedTableJSON.open('GET', 'scripts/JSON/tableData.json');
+        
+    /* onreadystatechange is causing the application to freeze when it didn't in the test one I have.
+    getSavedTableJSON.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Finished")
+        }
+        else {
+            alert(this.readyState + " " + this.status);
+        }
+    };
+    */
+    getSavedTableJSON.onload = function() {
+        var tableData = JSON.parse(getSavedTableJSON.responseText);
+        $('#primary1').text((JSON.stringify(tableData[0].server2)));
+    };
+    getSavedTableJSON.send();
+    alert("finished");
+    /* Old AJAX data that is working, but replacing with XMLHttpRequest
+    alert("clicked");
+    $.ajax({
+        url: "scripts/test.php",
+        type: "POST",
+        data: {message: "test"},
+        dataType: "JSON",
+        
+        success: function(jsonStr) {
+            $('#result').text(JSON.stringify(jsonStr));
+        }
+    });
+    alert("Finished");
+    */
+    });
+// Submit button that gets data from JSON file.
 $().ready(function() {
-    $('#jsSubmit').click(function() {
+    $('#jsSubmit').click(function () {
         //alert("Clicked");
 
-        
-        
-        var getSavedTableJSON = new XMLHttpRequest();
-        getSavedTableJSON.open('GET', 'scripts/JSON/tableData.json');
-        
-        /* onreadystatechange is causing the application to freeze when it didn't in the test one I have.
-        getSavedTableJSON.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                alert("Finished")
-            }
-            else {
-                alert(this.readyState + " " + this.status);
-            }
-        };
-        */
-        getSavedTableJSON.onload = function() {
-                var tableData = JSON.parse(getSavedTableJSON.responseText);
-                $('#primary1').text((JSON.stringify(tableData[0].server2)));
-        };
-        getSavedTableJSON.send();
-        alert("finished");
-        /* Old AJAX data that is working, but replacing with XMLHttpRequest
-        alert("clicked");
-        $.ajax({
-            url: "scripts/test.php",
-            type: "POST",
-            data: {message: "test"},
-            dataType: "JSON",
-            
-            success: function(jsonStr) {
-                $('#result').text(JSON.stringify(jsonStr));
-            }
-        });
-        alert("Finished");
-        */
+        SendDataModule();
     });
 });
+// Trying to send and save data to JSON file. 
 $().ready(function() {
     $('#jsGet').click(function() {
         // Was using these two lines to create JSON string out of the HTML table.
