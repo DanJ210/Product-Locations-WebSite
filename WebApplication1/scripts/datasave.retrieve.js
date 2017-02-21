337b83cb2fe3c9c14fdac7132749b566b579bce9
djackson <djackson@DJACKSON>
2017-02-21 17:17:07 -0500

    Added code to write from JSON file to tables.

--- WebApplication1/scripts/datasave.retrieve.js
+++ WebApplication1/scripts/datasave.retrieve.js
@@ -1,5 +1,12 @@
 /// <reference path="scripts/jquery-3.1.1.js"/>
 
+// Function that writes data to the table when button is clicked
+// Need to change this to work when the page is first loaded or refreshed.
+$(document).ready(function() {
+    $('#jsSubmit').click(function() {
+        writeTableData();
+    });
+});
 
 /**
     * 
@@ -7,16 +14,22 @@
     * @description Sends http request when event is triggered to GET data from JSON file.
     * @param 
 */
-$().ready(function() {
-    $('#jsSubmit').click(function() {
+function writeTableData() {
         //alert("Clicked");
 
+        var tables = ["firstProductTable", "secondProductTable"];
+        for (var i = 0; i < tables.length; i++) {
+            if (i === 0) {
+                var tableNumber = null;
+                requestFromJSONFile(tableNumber);
+            } else if(i === 1) {
+                var tableNumber = "2-";
+                requestFromJSONFile(tableNumber);
+            };
+        };
         
-        
-        var getSavedTableJSON = new XMLHttpRequest();
-        getSavedTableJSON.open('GET', 'scripts/JSON/tableData.json');
-        
-        /* onreadystatechange is causing the application to freeze when it didn't in the test one I have.
+        // onreadystatechange is causing the application to freeze when it didn't in the test one I have.
+        /*
         getSavedTableJSON.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 alert("Finished")
@@ -26,12 +39,63 @@ $().ready(function() {
             }
         };
         */
-        getSavedTableJSON.onload = function() {
+        function requestFromJSONFile(tableNumber) {
+            var getSavedTableJSON = new XMLHttpRequest();
+            // In this future this will change the file to which we want data from
+            if (tableNumber === null) {
+                getSavedTableJSON.open('GET', 'scripts/JSON/newTableData.json');
+            } else {
+                getSavedTableJSON.open('GET', 'scripts/JSON/newTableData.json');
+            };
+            
+            getSavedTableJSON.onload = function() {
                 var tableData = JSON.parse(getSavedTableJSON.responseText);
-                $('#primary1').text((JSON.stringify(tableData[0].server2)));
+                //$('#response').text(JSON.stringify(tableData));
+                //$('#primary1').text((JSON.stringify(tableData[0].server2)));
+
+                // Variable to control which TD element gets the data
+                var count = 0;
+                //sendDataToCell(getSavedTableJSON, tableNumber);
+                $.each(tableData, function(key, value) {
+                    if (tableNumber === null) {
+                        $('#primary'  + count).text(JSON.stringify(tableData[key].primary));
+                        $('#secondary'  +  count).text(JSON.stringify(tableData[key].secondary));
+                        count++;
+                    } else if(tableNumber === "2-") {
+                        $('#primary'  + tableNumber + count).text(JSON.stringify(tableData[key].primary));
+                        $('#secondary'  + tableNumber +  count).text(JSON.stringify(tableData[key].secondary));
+                        count++;
+                    };
+                });
+            };
+            getSavedTableJSON.send();
+            alert("finished");
         };
-        getSavedTableJSON.send();
-        alert("finished");
+        function sendDataToCell(getSavedTableJSON, tableNumber) {
+
+        }
+                /*
+            if (tableNumber === null) {
+                
+            } else if(tableSelect === "2-") {
+                getSavedTableJSON.open('GET', 'scripts/JSON/newTableData.json');
+                getSavedTableJSON.onload = function() {
+                    var tableData = JSON.parse(getSavedTableJSON.responseText);
+                    $('#response').text(JSON.stringify(tableData));
+                    //$('#primary1').text((JSON.stringify(tableData[0].server2)));
+
+                    // Variable to control which TD element gets the data
+                    var count = 0;
+                    $.each(tableData, function(key, value) {
+                        $('#primary' + tableSelect + count).text(JSON.stringify(tableData[key].primary));
+                        $('#secondary' + tableSelect + count).text(JSON.stringify(tableData[key].secondary));
+                        count++;
+                    });
+                };
+            };
+            */
+        
+        
         /* Old AJAX data that is working, but replacing with XMLHttpRequest
         alert("clicked");
         $.ajax({
@@ -46,19 +110,24 @@ $().ready(function() {
         });
         alert("Finished");
         */
-    });
-});
+    
+};
+
+// Creating JSON string out of the table, trying to save it to a file
 $().ready(function() {
     $('#jsGet').click(function() {
         // Was using these two lines to create JSON string out of the HTML table.
-        var table = $('#tableToSave').tableToJSON();
+        var table = $('#firstProductTable').tableToJSON();
         jsonTable = JSON.stringify(table);
         alert(jsonTable);
 
+        var table2 = $('#firstProductTable').tableToJSON();
+        alert(JSON.stringify(table2));
+        /*
         var sendTableDataJSON = new XMLHttpRequest();
         sendTableDataJSON.open('POST', 'scripts/JSON/newTableData.json');
         sendTableDataJSON.send(jsonTable);
-    
+        */
     });
 });
 
